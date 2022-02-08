7550045a09d80bf5fceed3be4441cdc6366b5c4a
Martin <martinchogimenez@gmail.com>
2022-02-08 20:45:10 -0300

    Fixing steps

--- src/test/java/ar/steps/Steps.java
+++ src/test/java/ar/steps/Steps.java
@@ -2,6 +2,10 @@ package ar.steps;
 
 import api.config.EntityConfiguration;
 import api.model.Hour;
+import ar.validator.GetValidator;
+import ar.validator.PostHValidator;
+import ar.validator.PutHValidator;
+
 import com.crowdar.api.rest.APIManager;
 import com.crowdar.core.PageSteps;
 import io.cucumber.java.en.*;
@@ -13,7 +17,7 @@ import services.BaseService;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Map;
 
-public class CommonSteps extends PageSteps {
+public class Steps extends PageSteps {
 
     @When("^I perform a '(.*)' to '(.*)' endpoint with the '(.*)' and '(.*)'$")
     public void doRequest(String methodName, String entity, String jsonName, String jsonReplacementValues)
@@ -42,6 +46,49 @@ public class CommonSteps extends PageSteps {
         Hour response = (Hour) APIManager.getLastResponse().getResponse();
         String x = response.getId();
         BaseService.ID_TIME_ENTRY.set(x);
+    }
+
+    @And("expected response is obtained.")
+    public void expectedResponseIsObtained() {
+        GetValidator.TimeValidator();
 
     }
+
+    @Given("Mi cuenta creada en clockify y mi X-Api-Key generada")
+    public void miCuentaCreadaEnClockifyYMiXApiKeyGenerada() {
+
+        BaseService.API_KEY.set("MmNiMjU0NWYtOTAwZi00NGRjLTlmNTUtODc5MTM5ZDRlZGIw");
+        BaseService.BASE_URL.set("https://api.clockify.me/api/v1");
+    }
+
+    @And("un id workspace valido")
+    public void unIdWorkspaceValido() {
+        BaseService.ID_WORKSPACE.set("617475360be7933405e795fb");
+    }
+
+    @And("^un nombre de proyecto (.*)$")
+    public void unNombreDeProyectoName(String name) {
+        BaseService.NAME.set(name);
+    }
+
+    @Then("Valido que haya cambiado el valor de la propiedad billable")
+    public void validoQueHayaCambiadoElValorDeLaPropiedadBillable() {
+        PutHValidator.validate();
+    }
+
+    @Then("Valido los datos obtenidos sean correctos.")
+    public void validoLosDatosObtenidosSeanCorrectos() {
+        PostHValidator.validate();
+    }
+
+    @And("un id project valido")
+    public void unIdProjectValido() {
+        BaseService.ID_PROJECT.set("61780c853d249a5ebc6dce67");
+    }
+
+    @And("un id de usuario valido")
+    public void unIdDeUsuarioValido() {
+        BaseService.ID_USER.set("616f3f8c99d16c30b318e495");
+    }
+
 }
